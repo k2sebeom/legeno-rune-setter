57bf25d753dc153836de5d7223700d84652ba16d
Randy Kim <randy.h.kim@vanderbilt.edu>
2022-02-02 01:49:06 +0900

    Demonstrate all precision runes

--- src/main/MainApplication/MainApplication.py
+++ src/main/MainApplication/MainApplication.py
@@ -12,15 +12,14 @@ class MainApplication(tk.Tk):
         self.geometry('900x600')
         self.resizable(False, False)
 
-        # TODO: remove borderwidth and bg; for debugging purpose only
-        _toolbar = tk.Frame(self, borderwidth=1, bg='cyan')
-        _left_runes = tk.Frame(self, borderwidth=1, bg='red')
-        _right_runes = tk.Frame(self, borderwidth=1, bg='blue')
+        _toolbar = tk.Frame(self)
+        _left_runes = tk.Frame(self)
+        _right_runes = tk.Frame(self)
 
         self.rowconfigure(0, weight=1, uniform='a')
         self.rowconfigure(1, weight=10, uniform='a')
         self.columnconfigure(0, weight=1, uniform='a')
-        self.columnconfigure(1,weight=1, uniform='a')
+        self.columnconfigure(1, weight=1, uniform='a')
 
         # Top
         _toolbar.grid(row=0, column=0, sticky='nsew', columnspan=2)
@@ -50,18 +49,176 @@ class MainApplication(tk.Tk):
 
         # Bottom Left
         _left_runes.grid(row=1, column=0, sticky='nsew')
-        _left_runes.rowconfigure(0, weight=1)
-        _left_runes.columnconfigure(0, weight=1)
-        # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-        _img = ImageTk.PhotoImage(Image.open("../resources/precision/Press_the_Attack_rune.png"))
-        _rounded_button = tk.Button(_left_runes, image=_img)
-        _rounded_button.photo = _img
-        # _rounded_button["bg"] = "white"
-        # _rounded_button["border"] = "0"
-        _rounded_button.grid(row=0, column=0)
+        _left_runes.columnconfigure(0, weight=1, uniform='a')
+        _left_runes.rowconfigure(0, weight=1, uniform='a')
+        _left_runes.rowconfigure(1, weight=1, uniform='a')
+        _left_runes.rowconfigure(2, weight=1, uniform='a')
+        _left_runes.rowconfigure(3, weight=1, uniform='a')
+        _left_runes.rowconfigure(4, weight=1, uniform='a')
+
+        _left_runes_row0 = tk.Frame(_left_runes)
+        _left_runes_row1 = tk.Frame(_left_runes)
+        _left_runes_row2 = tk.Frame(_left_runes)
+        _left_runes_row3 = tk.Frame(_left_runes)
+        _left_runes_row4 = tk.Frame(_left_runes)
+
+        _left_runes_row0.grid(row=0, column=0, sticky='nsew')
+        _left_runes_row1.grid(row=1, column=0, sticky='nsew')
+        _left_runes_row2.grid(row=2, column=0, sticky='nsew')
+        _left_runes_row3.grid(row=3, column=0, sticky='nsew')
+        _left_runes_row4.grid(row=4, column=0, sticky='nsew')
+
+        _left_runes_row0.rowconfigure(0, weight=1)
+        _left_runes_row0.columnconfigure(0, weight=1)
+        _left_runes_row0.columnconfigure(1, weight=1)
+        _left_runes_row0.columnconfigure(2, weight=1)
+        _left_runes_row0.columnconfigure(3, weight=1)
+        _left_runes_row0.columnconfigure(4, weight=1)
+
+        img = Image.open("../resources/precision/Precision_rune.png")
+        img_tk = ImageTk.PhotoImage(img)
+        _precision_button = tk.Button(_left_runes_row0, image=img_tk)
+        _precision_button.photo = img_tk
+        _precision_button.grid(row=0, column=0)
+
+        img = Image.open("../resources/domination/Domination_rune.png")
+        img = img.convert('L')
+        img_tk = ImageTk.PhotoImage(img)
+        _precision_button = tk.Button(_left_runes_row0, image=img_tk)
+        _precision_button.photo = img_tk
+        _precision_button.grid(row=0, column=1)
+
+        img = Image.open("../resources/sorcery/Sorcery_rune.png")
+        img = img.convert("L")
+        img_tk = ImageTk.PhotoImage(img)
+        _precision_button = tk.Button(_left_runes_row0, image=img_tk)
+        _precision_button.photo = img_tk
+        _precision_button.grid(row=0, column=2)
+
+        img = Image.open("../resources/resolve/Resolve_rune.png")
+        img = img.convert("L")
+        img_tk = ImageTk.PhotoImage(img)
+        _precision_button = tk.Button(_left_runes_row0, image=img_tk)
+        _precision_button.photo = img_tk
+        _precision_button.grid(row=0, column=3)
+
+        img = Image.open("../resources/inspiration/Inspiration_rune.png")
+        img = img.convert("L")
+        img_tk = ImageTk.PhotoImage(img)
+        _precision_button = tk.Button(_left_runes_row0, image=img_tk)
+        _precision_button.photo = img_tk
+        _precision_button.grid(row=0, column=4)
+
+        _left_runes_row1.rowconfigure(0, weight=1, uniform='a')
+        _left_runes_row1.columnconfigure(0, weight=1, uniform='a')
+        _left_runes_row1.columnconfigure(1, weight=1, uniform='a')
+        _left_runes_row1.columnconfigure(2, weight=1, uniform='a')
+
+        img = Image.open("../resources/precision/Press_the_Attack_rune.png")
+        img = img.resize((100, 100))
+        img_tk = ImageTk.PhotoImage(img)
+        _press_the_attack_button = tk.Button(_left_runes_row1, image=img_tk, border=0)
+        _press_the_attack_button.photo = img_tk
+        # _press_the_attack_button["bg"] = "white"
+        _press_the_attack_button.grid(row=0, column=0)
+
+        img = Image.open("../resources/precision/Lethal_Tempo_rune.png")
+        img = img.resize((100, 100))
+        img_tk = ImageTk.PhotoImage(img)
+        _lethal_tempo_button = tk.Button(_left_runes_row1, image=img_tk, border=0)
+        _lethal_tempo_button.photo = img_tk
+        _lethal_tempo_button.grid(row=0, column=1)
+
+        img = Image.open("../resources/precision/Fleet_Footwork_rune.png")
+        img = img.resize((100, 100))
+        img_tk = ImageTk.PhotoImage(img)
+        _lethal_tempo_button = tk.Button(_left_runes_row1, image=img_tk, border=0)
+        _lethal_tempo_button.photo = img_tk
+        _lethal_tempo_button.grid(row=0, column=2)
+
+        img = Image.open("../resources/precision/Conqueror.png")
+        img = img.resize((100, 100))
+        img_tk = ImageTk.PhotoImage(img)
+        _lethal_tempo_button = tk.Button(_left_runes_row1, image=img_tk, border=0)
+        _lethal_tempo_button.photo = img_tk
+        _lethal_tempo_button.grid(row=0, column=3)
+
+        _left_runes_row2.rowconfigure(0, weight=1)
+        _left_runes_row2.columnconfigure(0, weight=1)
+        _left_runes_row2.columnconfigure(1, weight=1)
+        _left_runes_row2.columnconfigure(2, weight=1)
+
+        img = Image.open("../resources/precision/Overheal_rune.png")
+        img = img.resize((50, 50))
+        img_tk = ImageTk.PhotoImage(img)
+        _lethal_tempo_button = tk.Button(_left_runes_row2, image=img_tk, border=0)
+        _lethal_tempo_button.photo = img_tk
+        _lethal_tempo_button.grid(row=0, column=0)
+
+        img = Image.open("../resources/precision/Triumph_rune.png")
+        img = img.resize((50, 50))
+        img_tk = ImageTk.PhotoImage(img)
+        _lethal_tempo_button = tk.Button(_left_runes_row2, image=img_tk, border=0)
+        _lethal_tempo_button.photo = img_tk
+        _lethal_tempo_button.grid(row=0, column=1)
+
+        img = Image.open("../resources/precision/Presence_of_Mind_rune.png")
+        img = img.resize((50, 50))
+        img_tk = ImageTk.PhotoImage(img)
+        _lethal_tempo_button = tk.Button(_left_runes_row2, image=img_tk, border=0)
+        _lethal_tempo_button.photo = img_tk
+        _lethal_tempo_button.grid(row=0, column=2)
+
+        _left_runes_row3.rowconfigure(0, weight=1)
+        _left_runes_row3.columnconfigure(0, weight=1)
+        _left_runes_row3.columnconfigure(1, weight=1)
+        _left_runes_row3.columnconfigure(2, weight=1)
+
+        img = ImageTk.PhotoImage(Image.open("../resources/precision/Legend-_Alacrity_rune.png"))
+        _lethal_tempo_button = tk.Button(_left_runes_row3, image=img, border=0)
+        _lethal_tempo_button.photo = img
+        _lethal_tempo_button.grid(row=0, column=0)
+
+        img = ImageTk.PhotoImage(Image.open("../resources/precision/Legend-_Tenacity_rune.png"))
+        _lethal_tempo_button = tk.Button(_left_runes_row3, image=img, border=0)
+        _lethal_tempo_button.photo = img
+        _lethal_tempo_button.grid(row=0, column=1)
+
+        img = ImageTk.PhotoImage(Image.open("../resources/precision/Legend-_Bloodline_rune.png"))
+        _lethal_tempo_button = tk.Button(_left_runes_row3, image=img, border=0)
+        _lethal_tempo_button.photo = img
+        _lethal_tempo_button.grid(row=0, column=2)
+
+        _left_runes_row4.rowconfigure(0, weight=1)
+        _left_runes_row4.columnconfigure(0, weight=1)
+        _left_runes_row4.columnconfigure(1, weight=1)
+        _left_runes_row4.columnconfigure(2, weight=1)
+
+        img = ImageTk.PhotoImage(Image.open("../resources/precision/Coup_de_Grace_rune.png"))
+        _lethal_tempo_button = tk.Button(_left_runes_row4, image=img, border=0)
+        _lethal_tempo_button.photo = img
+        _lethal_tempo_button.grid(row=0, column=0)
+
+        img = ImageTk.PhotoImage(Image.open("../resources/precision/Cut_Down_rune.png"))
+        _lethal_tempo_button = tk.Button(_left_runes_row4, image=img, border=0)
+        _lethal_tempo_button.photo = img
+        _lethal_tempo_button.grid(row=0, column=1)
+
+        img = ImageTk.PhotoImage(Image.open("../resources/precision/Last_Stand_rune.png"))
+        _lethal_tempo_button = tk.Button(_left_runes_row4, image=img, border=0)
+        _lethal_tempo_button.photo = img
+        _lethal_tempo_button.grid(row=0, column=2)
 
         # Bottom Right
         _right_runes.grid(row=1, column=1, sticky='nsew')
+        _right_runes.columnconfigure(0, weight=1)
+        _right_runes.rowconfigure(0, weight=2)
+        _right_runes.rowconfigure(1, weight=1)
+        _right_runes.rowconfigure(2, weight=1)
+        _right_runes.rowconfigure(3, weight=1)
+        _right_runes.rowconfigure(4, weight=1)
+        _right_runes.rowconfigure(5, weight=1)
+        _right_runes.rowconfigure(6, weight=1)
 
         # TODO: might be better if toolbar, left_runes, and right_runes are separate objects, and widgets are attributes
 
